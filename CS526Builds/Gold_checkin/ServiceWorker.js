13f5f2d0518fe1e4f374ece0ac871a255973977f
Shashank MG <37444395+ShashankMG123@users.noreply.github.com>
2024-07-16 17:49:09 -0700

    Try PWA

---
+++ CS526Builds/Gold_checkin/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Chasers_Gambit-0.1";
+const contentToCache = [
+    "Build/Gold_checkin.loader.js",
+    "Build/Gold_checkin.framework.js.unityweb",
+    "Build/Gold_checkin.data.unityweb",
+    "Build/Gold_checkin.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
